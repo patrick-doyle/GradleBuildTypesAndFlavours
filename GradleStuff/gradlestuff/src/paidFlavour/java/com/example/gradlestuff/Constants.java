73af5a5bb7c596e717921cee072edc2f7697f640
Patrick Doyle <patrick@twsitedequations.com>
2014-02-03 20:41:55 +0000

    Inital coimmit

---
+++ GradleStuff/gradlestuff/src/paidFlavour/java/com/example/gradlestuff/Constants.java
@@ -0,0 +1,13 @@
+package com.example.gradlestuff;
+
+/**
+ * Created by Patrick on 02/02/14.
+ */
+public class Constants {
+
+    public enum Type {
+        FREE, PAID;
+    }
+
+    public static final Type type = Type.PAID;
+}
